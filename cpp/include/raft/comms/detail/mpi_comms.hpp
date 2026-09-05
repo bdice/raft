51a23751650900af9fff55d1a9acf420feac542d
Bradley Dice <bdice@bradleydice.com>
2026-09-05 18:19:12 -0500

    Extract native handles at CUDA stream boundaries

--- cpp/include/raft/comms/detail/mpi_comms.hpp
+++ cpp/include/raft/comms/detail/mpi_comms.hpp
@@ -103,7 +103,7 @@ class mpi_comms : public comms_iface {
       rank_(1),
       status_(stream),
       next_request_id_(0),
-      stream_(stream)
+      stream_(stream.get())
   {
     int mpi_is_initialized = 0;
     RAFT_MPI_TRY(MPI_Initialized(&mpi_is_initialized));
@@ -134,7 +134,7 @@ class mpi_comms : public comms_iface {
       rank_(1),
       status_(stream),
       next_request_id_(0),
-      stream_(stream)
+      stream_(stream.get())
   {
     int mpi_is_initialized = 0;
     RAFT_MPI_TRY(MPI_Initialized(&mpi_is_initialized));
--- cpp/include/raft/solver/linear_assignment.cuh
+++ cpp/include/raft/solver/linear_assignment.cuh
@@ -178,7 +178,7 @@ class LinearAssignmentProblem {
     weight_t result;
     raft::update_host(
       &result, obj_val_primal_v.data() + spId, 1, resource::get_cuda_stream(handle_));
-    RAFT_CHECK_CUDA(resource::get_cuda_stream(handle_));
+    RAFT_CHECK_CUDA(resource::get_cuda_stream(handle_).get());
     return result;
   }
 
