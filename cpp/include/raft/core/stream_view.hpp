c569fb6c8fa499258da3971522d251d0c86e378d
Bradley Dice <bdice@bradleydice.com>
2026-09-06 13:55:56 -0500

    Complete stream compatibility preparation

--- cpp/include/raft/core/stream_view.hpp
+++ cpp/include/raft/core/stream_view.hpp
@@ -29,7 +29,10 @@ struct fail_stream_view {
   constexpr fail_stream_view(fail_stream_view&&)                         = default;
   auto constexpr operator=(fail_stream_view const&) -> fail_stream_view& = default;
   auto constexpr operator=(fail_stream_view&&) -> fail_stream_view&      = default;
-  auto value() { throw non_cuda_build_error{"Attempted to access CUDA stream in non-CUDA build"}; }
+  auto value() const
+  {
+    throw non_cuda_build_error{"Attempted to access CUDA stream in non-CUDA build"};
+  }
   [[nodiscard]] auto is_per_thread_default() const { return false; }
   [[nodiscard]] auto is_default() const { return false; }
   void synchronize() const
@@ -70,7 +73,7 @@ struct stream_view {
   constexpr stream_view(stream_view&&)               = default;
   auto operator=(stream_view const&) -> stream_view& = default;
   auto operator=(stream_view&&) -> stream_view&      = default;
-  auto value() { return base_view_.value(); }
+  auto value() const { return base_view_.value(); }
   operator underlying_view_type() const noexcept { return base_view_; }
   [[nodiscard]] auto is_per_thread_default() const { return base_view_.is_per_thread_default(); }
   [[nodiscard]] auto is_default() const { return base_view_.is_default(); }
